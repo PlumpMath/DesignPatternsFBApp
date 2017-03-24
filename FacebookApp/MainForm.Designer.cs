4c3dd94d30b8ba17275fbc20790ffcf0b97e7573
barvazos <amitaihandler@gmail.com>
2017-03-24 22:24:10 +0300

    Very basic start
    basically tried out the  demo from class, with our very own app id.
    currently only has one feature of displaying the liked pages in a list
    box

---
+++ FacebookApp/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FacebookApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.likedPagesListBox = new System.Windows.Forms.ListBox();
+            this.likedPagesLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(12, 12);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(93, 12);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogout.TabIndex = 1;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // likedPagesListBox
+            // 
+            this.likedPagesListBox.FormattingEnabled = true;
+            this.likedPagesListBox.ItemHeight = 16;
+            this.likedPagesListBox.Location = new System.Drawing.Point(16, 62);
+            this.likedPagesListBox.Name = "likedPagesListBox";
+            this.likedPagesListBox.Size = new System.Drawing.Size(227, 180);
+            this.likedPagesListBox.TabIndex = 2;
+            // 
+            // likedPagesLinkLabel
+            // 
+            this.likedPagesLinkLabel.AutoSize = true;
+            this.likedPagesLinkLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.likedPagesLinkLabel.Location = new System.Drawing.Point(13, 42);
+            this.likedPagesLinkLabel.Name = "likedPagesLinkLabel";
+            this.likedPagesLinkLabel.Size = new System.Drawing.Size(107, 17);
+            this.likedPagesLinkLabel.TabIndex = 3;
+            this.likedPagesLinkLabel.TabStop = true;
+            this.likedPagesLinkLabel.Text = "Get liked pages";
+            this.likedPagesLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.likedPages_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 486);
+            this.Controls.Add(this.likedPagesLinkLabel);
+            this.Controls.Add(this.likedPagesListBox);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.ListBox likedPagesListBox;
+        private System.Windows.Forms.LinkLabel likedPagesLinkLabel;
+    }
+}
+
---
+++ FacebookApp/MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace FacebookApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = k_CollectionLimit;
+            FacebookWrapper.FacebookService.s_FbApiVersion = k_FbApiVersion;
+        }
+
+        User m_LoggedInUser;
+        private const int k_CollectionLimit = 200;
+        private const float k_FbApiVersion = 2.8f;
+
+        private void loginAndInit()
+        {
+            LoginResult result = FacebookService.Login("1470264299671719",
+                "public_profile",
+                "user_birthday",
+                "user_friends",
+                "user_likes",
+                "user_location",
+                "user_photos",
+                "user_posts"
+                );
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            logoutAndReset();
+        }
+
+        private void logoutAndReset()
+        {
+            //TODO reset the page (and record user settings?)
+        }
+
+        private void getLikedPages()
+        {
+            likedPagesListBox.Items.Clear();
+            likedPagesListBox.DisplayMember = "Name";
+
+            foreach (Page page in m_LoggedInUser.LikedPages)
+            {
+                likedPagesListBox.Items.Add(page);
+            }
+
+            if (m_LoggedInUser.LikedPages.Count == 0)
+            {
+                MessageBox.Show("You have no liked pages to show");
+            }
+        }
+
+        private void likedPages_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            getLikedPages();
+        }
+              
+
+    }
+}
